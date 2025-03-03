fd1a343afb7ab3a2b265d91ea787490dbe65e682
Doougmooraes <dkcgamesstudio@gmail.com>
2025-03-03 12:09:28 -0300

    first comit

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ document.getElementById('loginBtn').addEventListener('click', () => {
         return;
     }
     
-
     // Verificar se o usuário e senha estão corretos
     const user = users.find(u => u.username === inputUsername && u.password === inputPassword);
 
